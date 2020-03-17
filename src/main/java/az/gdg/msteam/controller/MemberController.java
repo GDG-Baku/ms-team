d97c206ed21a0b4774159cfd291dc516c5352a77
asifhajiyev <asifhaciyev1498@gmail.com>
2020-03-17 23:15:02 +0400

    BE-2: unnecessary imports are removed

--- src/main/java/az/gdg/msteam/controller/MemberController.java
+++ src/main/java/az/gdg/msteam/controller/MemberController.java
@@ -5,7 +5,16 @@ import az.gdg.msteam.service.impl.MemberServiceImpl;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("members")
@@ -18,27 +27,27 @@ public class MemberController {
 
     @ApiOperation(value = "Getting all team members", response = MemberDto.class)
     @GetMapping
-    public ResponseEntity getAllMembers() {
-        return new ResponseEntity(memberService.getAllMembers(), HttpStatus.OK);
+    public ResponseEntity<List<MemberDto>> getAllMembers() {
+        return new ResponseEntity<>(memberService.getAllMembers(), HttpStatus.OK);
     }
 
     @PostMapping
-    public ResponseEntity addMember(@RequestBody MemberDto memberDto) {
-        return new ResponseEntity(memberService.createMember(memberDto), HttpStatus.OK);
+    public ResponseEntity<String> addMember(@RequestBody MemberDto memberDto) {
+        return new ResponseEntity<>(memberService.createMember(memberDto), HttpStatus.OK);
     }
 
     @DeleteMapping("{id}")
-    public ResponseEntity deleteMember(@PathVariable("id") Integer id) {
-        return new ResponseEntity(memberService.deleteMember(id), HttpStatus.OK);
+    public ResponseEntity<String> deleteMember(@PathVariable("id") Integer id) {
+        return new ResponseEntity<>(memberService.deleteMember(id), HttpStatus.OK);
     }
 
     @PutMapping("{id}")
-    public ResponseEntity updateMember(@PathVariable Integer id, @RequestBody MemberDto memberDto) {
-        return new ResponseEntity(memberService.updateMember(id, memberDto), HttpStatus.OK);
+    public ResponseEntity<String> updateMember(@PathVariable Integer id, @RequestBody MemberDto memberDto) {
+        return new ResponseEntity<>(memberService.updateMember(id, memberDto), HttpStatus.OK);
     }
 
     @GetMapping("{id}")
-    public ResponseEntity getMemberById(@PathVariable Integer id) {
-        return new ResponseEntity(memberService.getMemberById(id), HttpStatus.OK);
+    public ResponseEntity<MemberDto> getMemberById(@PathVariable Integer id) {
+        return new ResponseEntity<>(memberService.getMemberById(id), HttpStatus.OK);
     }
 }
--- src/main/java/az/gdg/msteam/model/entity/Member.java
+++ src/main/java/az/gdg/msteam/model/entity/Member.java
@@ -3,8 +3,11 @@ package az.gdg.msteam.model.entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import javax.persistence.*;
+import javax.persistence.Table;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 
 @Data
 @AllArgsConstructor
