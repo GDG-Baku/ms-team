08288f72005d22228aa72c9c3e493dba3c98fe5a
asifhajiyev <asifhaciyev1498@gmail.com>
2020-03-17 18:53:24 +0400

    delete all files from dev

--- src/main/java/az/gdg/msteam/repository/MemberRepository.java
+++
@@ -1,7 +0,0 @@
-package az.gdg.msteam.repository;
-
-import az.gdg.msteam.model.entity.Member;
-import org.springframework.data.repository.CrudRepository;
-
-public interface MemberRepository extends CrudRepository<Member, Long> {
-}
--- src/main/java/az/gdg/msteam/service/MemberService.java
+++
@@ -1,9 +0,0 @@
-package az.gdg.msteam.service;
-
-import az.gdg.msteam.model.dto.MemberDto;
-
-import java.util.List;
-
-public interface MemberService {
-    List<MemberDto> getAllMembers();
-}
