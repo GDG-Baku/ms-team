c2e4bea753581de046d834626ca3a45359f2f903
asifhajiyev <asifhaciyev1498@gmail.com>
2020-04-25 01:06:37 +0400

    BE-2: adding HealthController

---
+++ src/main/java/az/gdg/msteam/controller/HealthController.java
@@ -0,0 +1,24 @@
+package az.gdg.msteam.controller;
+
+import io.swagger.annotations.ApiOperation;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health")
+public class HealthController {
+    private static final Logger logger = LoggerFactory.getLogger(HealthController.class);
+
+
+    @ApiOperation(value = "Method will be called by ms-alarm")
+    @CrossOrigin(exposedHeaders = "Access-Control-Allow-Origin")
+    @GetMapping
+    public void health() {
+        logger.info("ActionLog.ms-article.start");
+    }
+}
